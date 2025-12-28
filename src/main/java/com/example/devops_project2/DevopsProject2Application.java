82fd3a68022e59165d836a560011274489d434db
rvm200584 <muthuccna@gmail.com>
2025-12-28 19:41:50 +0530

    java code updated

--- src/main/java/com/example/devops_project2/DevopsProject2Application.java
+++ src/main/java/com/example/devops_project2/DevopsProject2Application.java
@@ -2,12 +2,19 @@ package com.example.devops_project2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DevopsProject2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsProject2Application.class, args);
 	}
+	@GetMapping("/")
+	public String home() {
+	return "<h1>Welcome TCS DevOps Architect</h1>";
+	}	
 
 }

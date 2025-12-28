a9c8d6f2c32308fe56efd4af2f87451771235c42
rvm200584 <muthuccna@gmail.com>
2025-12-28 18:31:42 +0530

    new initial commit for project2

---
+++ src/main/java/com/example/devops_project2/DevopsProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.devops_project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devops_project2/DevopsProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
